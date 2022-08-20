eedcec4b7a0b5bcdc7756b51b250c0b6ae563991
Sumanbitw <007sumanghosh@gmail.com>
2022-08-20 10:04:30 +0530

    refactor

--- Week-4/solution-4.3.js
+++ Week-4/solution-4.3.js
@@ -1,15 +1,14 @@
 // Iterators 
 
-const FibObj = {
-  n: 6,
+const FibObj = (n) => ({
   [Symbol.iterator]: function () {
     let a = 0;
     let b = 0;
     let i = 1;
     return {
       next: () => {
-        if (i++ < this.n) {
-          [a, b] = [b, a + b || 1] //0, 1, 1, 2, 3
+        if (i++ < n) {
+          [a, b] = [b, a + b || 1] 
           return { value: a, done: false }
         } else {
           return { done: true }
@@ -17,7 +16,8 @@ const FibObj = {
       }
     }
   }
-}
-for (let num of FibObj) {
+});
+
+for (let num of FibObj(6)) {
   console.log(num)
 }
