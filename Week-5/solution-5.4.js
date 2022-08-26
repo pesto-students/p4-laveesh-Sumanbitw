364373aef675ab7abc2c590a1e958607f7bd8aef
Sumanbitw <007sumanghosh@gmail.com>
2022-08-26 23:09:28 +0530

    solution of Week-5.4 assignment

---
+++ Week-5/solution-5.4.js
@@ -0,0 +1,15 @@
+const mathOperation = {
+  sum: function(a,b){
+    return a + b;
+  },
+
+  difference: function(a,b){
+    return a - b;
+  },
+
+  product: function(a,b){
+    return a * b;
+  }
+}
+
+module.exports = mathOperation;
