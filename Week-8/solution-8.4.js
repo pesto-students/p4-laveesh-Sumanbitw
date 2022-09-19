22e6e8283fe21111129c35eb2c7622a183a3d25a
Sumanbitw <007sumanghosh@gmail.com>
2022-09-20 00:07:22 +0530

    solution-8.4

---
+++ Week-8/solution-8.4.js
@@ -0,0 +1,28 @@
+//Find if path exists in graph
+
+var validPath = function (_, edges, source, destination) {
+  if (source === destination) return true;
+
+  const uf = {};
+  for (const [u, v] of edges) {
+    if (uf[u] == undefined) uf[u] = new Set();
+    if (uf[v] == undefined) uf[v] = new Set();
+
+    uf[u].add(v);
+    uf[v].add(u);
+  }
+
+  const visited = new Set();
+
+  const dfs = (x, target) => {
+    if (visited.has(x) || !(x in uf)) return false;
+    visited.add(x);
+    const neighbours = uf[x];
+    if (neighbours.has(target)) return true;
+    return Array.from(neighbours).some((neighbour) => dfs(neighbour, target));
+  };
+
+  return dfs(source, destination);
+};
+
+console.log(validPath(3, [[0,1],[1,2],[2,0]], 0, 2))
